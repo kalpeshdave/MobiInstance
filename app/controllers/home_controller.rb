0f34cfdbc56d301c162d3455aee4137758d3cf92
Kalpesh Dave <kalpeshdave@foundershookup.com>
2015-06-05 13:55:55 +0530

    change favicon ico

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,15 @@
 class HomeController < ApplicationController
   before_filter :load_resources
   def index
-    @alchemist_resource = @resources.where(:name => "Alchemist Accelerator")
+    @alchemist_resource = @all_resources.where(:name => "Alchemist Accelerator")
+    @resources = @all_resources - @alchemist_resource
     @requested_user = RequestedUser.new
   end
 
   private
 
   def load_resources
-    @resources = @instance.resources
-    redirect_to public_index_url(subdomain: 'www') if @resources.blank?
+    @all_resources = @instance.resources
+    redirect_to public_index_url(subdomain: 'www') if @all_resources.blank?
   end
 end
